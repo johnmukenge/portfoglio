2424ab9561db60c72a9075a64f5a2c74b15a0012
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-12-13 19:20:21 +0100

    created sections as components

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { ReactDOM } from "react-dom";
+import App from "./App"; // import the App component
+import "./index.css"; // import the stylesheet
+
+ReactDOM.render(<App />, document.getElementById("root")); // render the App component in the DOM
