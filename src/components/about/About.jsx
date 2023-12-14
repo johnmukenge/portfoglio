3619f4ca07bfefbd92d4e01d499ac18143720f83
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-12-14 20:43:59 +0100

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me2.jpeg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const about = () => {
   return (
     <section id='about'>
-      About
+      <h5>Tutto</h5>
+      <h2>su di me</h2>
+      <div className='about-container'>
+        <div className='about-me'>
+          <div className='about-me-image'>
+            <img src={ME} alt='about-me' />
+          </div>
+        </div>
+        <div className='about-content'>
+          <div className='about-cards'>
+            <article className='about-card'>
+              <FaAward className='about-icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years</small>
+            </article>
+            <article className='about-card'>
+              <FiUsers className='about-icon'/>
+              <h5>Clients</h5>
+              <small>200+ Clients Worldwide</small>
+            </article>
+            <article className='about-card'>
+              <VscFolderLibrary className='about-icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed projects</small>
+            </article>
+          </div>
+          <p>
+            lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Contattami</a>
+        </div>
+      </div>
     </section>
   )
 }
