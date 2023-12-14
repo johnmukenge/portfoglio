5dccb3c6f264480463463699c46e629b684844f1
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-12-14 19:15:17 +0100

    floating nav component

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -4,11 +4,12 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
+import './navbar.css'
 
 const NavBar = () => {
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
+      <a href="#" className='active'><AiOutlineHome /></a>
       <a href="#about"><AiOutlineUser /></a>
       <a href="#experience"><BiBook /></a>
       <a href="#services"><RiServiceLine /></a>
