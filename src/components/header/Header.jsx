37fd3c7cd45f175241801e439091cce3ba9e2eaf
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-12-14 18:12:51 +0100

    header's component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <h1>
-      Header
-    </h1>
+    <header>
+      <div className="container header_container">
+        <h5>Ciao Sono</h5>
+        <h1>John Mukenge</h1>
+        <h5 className="text-light">Sono un Software Engineer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt='me'/>
+        </div>
+        <a href="#contact" className="scroll_down" >Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+      <a href='https://www.linkedin.com/in/john-mukenge/' target='_blank' rel='noreferrer'></a>
+      <a href='https://github.com/johnmukenge' target='_blank' rel='noreferrer'></a>
+      <a href='https://twitter.com/johnmukenge' target='_blank' rel='noreferrer'></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
