ed39b1c16a50b2e96cec86981d0447dab67beffc
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-12-14 18:37:42 +0100

    header component css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials';
+import './header.css'
 
 const Header = () => {
   return (
